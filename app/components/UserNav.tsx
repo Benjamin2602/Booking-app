cf3405349d529c42cd3863c115e9f4e01c7ea68f
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2024-02-24 06:08:55 +0530

    ✨ feat : kinde Auth

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -1,14 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 import {
   DropdownMenu,
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
-
 } from "@/components/ui/dropdown-menu";
 
 import { Menu, MenuIcon } from "lucide-react";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function UserNav() {
+export async function UserNav() {
+  const { getUser } = getKindeServerSession();
+  //fetching user data
+  const user = await getUser();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -16,16 +26,33 @@ export function UserNav() {
           <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
 
           <img
-            src="https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg"
+            src={
+              user?.picture ??
+              "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg"
+            }
             alt="Image"
             className="w-8 h-8 lg:w-7 hidden lg:block rounded-full"
           />
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[200px]">
-        <DropdownMenuItem>Register</DropdownMenuItem>
-        <DropdownMenuItem>login</DropdownMenuItem>
-
+        {/* if user signed in then it shows the logout dropmenu  */}
+        {user ? (
+          <>
+            <DropdownMenuItem>
+              <LogoutLink className="w-full">Logout</LogoutLink>
+            </DropdownMenuItem>
+          </>
+        ) : (
+          <>
+            <DropdownMenuItem>
+              <RegisterLink className="w-full">Register</RegisterLink>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <LoginLink className="w-full">Login</LoginLink>
+            </DropdownMenuItem>
+          </>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
