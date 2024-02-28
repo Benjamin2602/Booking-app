6de295f4a434c8b3817f669c3e322ed2a02a6458
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2024-02-28 09:44:35 +0530

    🚧 ✨ feat : description page for selecting category (form)

--- app/create/[id]/description/page.tsx
+++ app/create/[id]/description/page.tsx
@@ -1,7 +1,87 @@
-export default function DescriptionPage(){
-    return(
-        <div>
-            <h1>hello man from desc</h1>
+import { BottomBar } from "@/app/components/BottomBar";
+import { Counter } from "@/app/components/Counter";
+import { Card, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function DescriptionPage() {
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors">
+          please describe your home as good as you can!
+        </h2>
+      </div>
+      <form>
+        <div className="w-3/5 mx-auto mt-10 flex flex-col gap-y-5 mb-36">
+          <div className="flex flex-col gap-y-2">
+            <Label>Title</Label>
+            <Input name="title" required placeholder="short and simple...." />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Description</Label>
+            <Textarea
+              name="description"
+              required
+              placeholder="describe your home...."
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Price</Label>
+            <Input
+              name="price"
+              required
+              type="number"
+              placeholder="Price per night in INR"
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Image</Label>
+            <Input
+              name="image"
+              required
+              type="file"
+              accept="image/*"
+              placeholder="upload image"
+            />
+          </div>
+          <Card>
+            <CardHeader className="flex flex-col gap-y-5">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium">Guests</h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many guests would you like to bring
+                  </p>
+                </div>
+                <Counter />
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium">Rooms</h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many rooms would you like to have
+                  </p>
+                </div>
+                <Counter />
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium">Bathroom</h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many bathrooms would you like to have
+                  </p>
+                </div>
+                <Counter />
+              </div>
+            </CardHeader>
+          </Card>
         </div>
-    )
+        <BottomBar />
+      </form>
+    </>
+  );
 }
--- app/create/[id]/structure/page.tsx
+++ app/create/[id]/structure/page.tsx
@@ -1,9 +1,6 @@
 import { createCategoryPage } from "@/app/action";
+import { BottomBar } from "@/app/components/BottomBar";
 import { SelectCategory } from "@/app/components/SelectCategory";
-import { CreationSubmit } from "@/app/components/submitButton";
-import { categoryItems } from "@/app/lib/categoryItems";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export default function StructureRoute({ params }: { params: { id: string } }) {
   return (
@@ -17,14 +14,7 @@ export default function StructureRoute({ params }: { params: { id: string } }) {
       <form action={createCategoryPage}>
         <input type="hidden" name="homeId" value={params.id} />
         <SelectCategory />
-        <div className="fixed w-full bottom-0 z-10 bg-white border-t h-24">
-          <div className="flex items-center justify-between mx-auto px-5 lg:px-10 h-full">
-            <Button variant="secondary" size="lg" asChild>
-              <Link href="/">Cancel</Link>
-            </Button>
-            <CreationSubmit />
-          </div>
-        </div>
+        <BottomBar />
       </form>
     </>
   );
